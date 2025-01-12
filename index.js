7296c25d4b0b41a7d7462ede29b83f66327b6c09
ranjithpn292 <ranjithpn292@gmail.com>
2025-01-12 19:05:33 +0530

    added post requests

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json")
+const fs = require('fs')
 const app = express()
 const PORT = 8080
+
+// Middleware plugin
+app.use(express.urlencoded({ extended : false})); 
+
+app.use((req,res,next) => {
+  console.log(" hello from middleware 1");
+  req.hostname = "www.youtube.com";
+  req.path = "";
+  console.log("host and paths",req.path,req.hostname)
+  fs.appendFile("logfile.txt", `${Date.now()} , ${req.method} : ${ req.path} ${req.hostname} \n`, (err, data)=> {
+  next();
+  })
+})
 
 //ROUTES
 // for websites just return JSON
@@ -25,6 +39,14 @@ app.get("/api/users/:id",(req,res)=>{
   const id = req.params.id;
   const user = users.find((user)=> user.id == id)
   return res.json(user);
+})
+
+app.post("/api/users",(req,res)=>{
+  const body = req.body;
+  users.push({...body, id: users.length+1})
+  fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data)=>{
+    return res.status(201).json({status: "USER ADDED"});
+  })
 })
 
 
