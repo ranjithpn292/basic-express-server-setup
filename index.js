9f05e5ec4dceb1b258f1e0f965ac0fd40ab325ed
ranjithpn292 <ranjithpn292@gmail.com>
2025-01-12 11:27:46 +0530

    get requests added

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-const http = require('http')
-const fs =  require("fs")
+const express = require("express");
+const users = require("./MOCK_DATA.json")
+const app = express()
+const PORT = 8080
 
-const myServer = http.createServer((req,res)=>{
-    fs.appendFile("log.txt", `${new Date()} : ${req.url} new request recieved\n`,(err,data)=>{
-        switch(req.url){
-          case "/":
-            res.end("Home")
-            break
-          case "/about":
-            res.end(" Im ranjith")
-            break;
-         default:
-            res.end("404")
-        }
-    })
-});
+//ROUTES
+// for websites just return JSON
+app.get("/users",(req,res)=>{
+  const html = `
+  <ul>
+    ${users.map((user)=> (`<li>${user.first_name}</li>`)).join("")}
+  </ul>
+  `
+  return res.send(html);
+})
 
-myServer.listen(8000,()=>{
-    console.log("server is listening on port:" , )
+
+
+// for mobiles append /api/ return JSON
+app.get("/api/users",(req,res)=>{
+  return res.json(users);
+})
+
+app.get("/api/users/:id",(req,res)=>{
+  const id = req.params.id;
+  const user = users.find((user)=> user.id == id)
+  return res.json(user);
+})
+
+
+
+
+app.listen(PORT, ()=>{
+  console.log(`server is listening on PORT: ${PORT}`)
 })
